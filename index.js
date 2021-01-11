495d1a89b9b14754671d3535e6dd17568aaaf54a
Manoj9400 <77057128+Manoj9400@users.noreply.github.com>
2021-01-11 19:15:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
+var emailValidator = require("email-validator")
 const app = express();
 var port = 3000;
 let detail = require("./model");
+
 const router = express.Router();
+require('dotenv').config()
 
+app.use(bodyParser.json()); // for parsing application/json
+//console.log(bodyParser)
 app.use("/api", router);
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
 
-
-var uri = "mongodb://127.0.0.1:27017/book";
+app.use(express.json())
+// app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+var uri = process.env.MONGOURI;
 
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
@@ -19,28 +31,53 @@ connection.once("open", function() {
 });
 
 
-router.route("/insertdata").post(function(req, res) {
-  detail.insertMany(
-    [
-      { name: "Scooby" },
-      { age: 5 },
-      { breed: "Great Dane" },
-      { name: "Rambo" },
-      { age: 2 },
-      { breed: "Pitbull" },
-      { name: "Johny boy" },
-      { age: 3 },
-      { breed: "German Shephard" }
-    ],
-    function(err, result) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send(result);
-      }
+ if (emailValidator.validate("test@email.com")){
+   console.log("Email is valid")
+ }else{
+   console.log("Email is Invalid")
+ }
+
+router.route("/insertdata").post(function(req, response) {
+   //console.log(req)
+  //console.log(req.body.age)
+  var email = req.body.email
+  var name = req.body.name
+  var age = req.body.age
+  var breed = req.body.breed
+  if(!email){
+    return response.status(400).json({sucess:false ,message:"Email required."})
+  }  
+  
+  var myobj = { name:name, age:age, breed:breed, email:email };
+  
+  detail.create(myobj, function(err, res) {
+    console.log('Analyzing Data...');
+    detail.find({email:req.body.email}).then(res => {
+      // console.log(res)
+    if(res.length!=0) {
+        console.log('Your data has been successfully saved.');
+        //return response.json({data:res});
+        return response.json({
+          data:[],
+          success:false,
+          msg:"Email Exist"
+        })
     }
-  );
+    else{
+      detail.create(myobj)
+      console.log('Something went wrong while saving data.');
+      console.log(err);
+      //response.send(err);
+      return response.json({
+      data:[],
+          success:true,
+          msg:"Registered Successfuly"
+      })
+    }
+})
+  })
 });
+
 
 router.route("/get_data").get(function(req, res) {
   detail.find(
@@ -54,22 +91,15 @@ router.route("/get_data").get(function(req, res) {
   );
 });
 
-
 router.route("/single_data").post(function(req, res) {
   // console.log(req.{}.name);
   name = req.param('name')
   console.log(name)
-
-
  
   detail.findOne({name:name}).exec(function (err, data){
     return res.json(data)
   })
 });
-
-
-
-
 
 app.listen(port, function() {
   console.log("Server is running on Port: " + port);
